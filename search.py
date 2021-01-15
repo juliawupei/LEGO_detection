af8ed6b325603fef933fde2a24aa170a072b360c
juliawupei <juliawu0717@gmail.com>
2021-01-15 23:00:59 +0800

    initialize

--- search.py
+++ search.py
@@ -1,29 +1,31 @@
 import cv2
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 from msrest.authentication import ApiKeyCredentials
+from picamera import PiCamera
+from time import sleep
 
-camera = cv2.VideoCapture(0)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+camera = PiCamera()
+camera.resolution = (640,480)
 
-credentials = ApiKeyCredentials(in_headers={"Prediction-key": "<Prediction-key>"})
+credentials = ApiKeyCredentials(in_headers={"Prediction-key": "4a065daf45d9493481150079bcecedd9"})
 predictor = CustomVisionPredictionClient("https://southcentralus.api.cognitive.microsoft.com/", credentials)
 
-ret, image = camera.read()
-cv2.imwrite('capture.png', image)
+camera.capture('/home/pi/flask/capture_search.png')
+image = cv2.imread('capture_search.png')
+cv2.imwrite('capture_search.png', image)
 
-with open("capture.png", mode="rb") as captured_image:
-    results = predictor.detect_image("<Project-id>", "<Name-of-iteration", captured_image)
+with open("capture_search.png", mode="rb") as captured_image:
+    results = predictor.detect_image("1b347900-c97b-4769-88bf-266ebfa913b7", "Iteration7", captured_image)
 
 for prediction in results.predictions:
-    if prediction.probability > 0.5 :
+    if prediction.probability > 0.4 :
 
         bbox = prediction.bounding_box
 
         cv2.putText(image, prediction.tag_name, (int(bbox.left * 640), int(bbox.top * 480)-4), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
         result_image = cv2.rectangle(image, (int(bbox.left * 640), int(bbox.top * 480)), (int((bbox.left + bbox.width) * 640), int((bbox.top + bbox.height) * 480)), (0, 255, 0), 1)
-        cv2.imwrite('result.png', result_image)
+        cv2.imwrite('result_search.png', result_image)
 
 
-camera.release()
+camera.close()
